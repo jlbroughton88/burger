b61b545da9122c1846f9a06ab91a9b909ef81b4d
jlb1999 <JacobBroughton@JacobBroughtonsMacbook.local>
2018-10-22 22:50:18 -0400

    revised commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,16 @@ VALUES ("The NC Steak Burger",false);
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("The Minnisota Flame", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Callifornia Classic", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Slim Jimmy", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Roasted Cow", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Patty Melt", false);
+
